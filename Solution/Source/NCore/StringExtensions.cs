20c7586d848c462e3f8905d2dee425617e5fb7b5
Daniel Wertheim <daniel@wertheim.se>
2011-08-10 11:44:46 +0200

    [New]: Split string with KeyValues to Dictionary.

--- Solution/Source/NCore/StringExtensions.cs
+++ Solution/Source/NCore/StringExtensions.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace NCore
 {
@@ -14,6 +17,13 @@ namespace NCore
         public static string Inject(this string format, params string[] formattingArgs)
         {
             return string.Format(format, formattingArgs);
+        }
+
+        public static IDictionary<string, string> ToKeyValues(this string keyValueString, char pairDelim, char keyValueDelim)
+        {
+            return keyValueString.Split(new[] { pairDelim }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(kv => kv.Split(new[] { keyValueDelim }, StringSplitOptions.RemoveEmptyEntries))
+                .ToDictionary(kv => kv[0], kv => kv[1]);
         }
     }
 }
---
+++ Solution/Tests/NCore.Tests.UnitTests/StringExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace NCore.Tests.UnitTests
+{
+    [TestFixture]
+    public class StringExtensionsTests : UnitTestBase
+    {
+        [Test]
+        public void ToKeyValues_WhenTwoKeyValuePairsExists_TwoKeyValueEntriesAreCreated()
+        {
+            var stringValue = "Key1=Value1;Key2=Value2";
+
+            var keyValues = stringValue.ToKeyValues(';', '=');
+
+            Assert.AreEqual(2, keyValues.Count);
+            Assert.AreEqual(keyValues["Key1"], "Value1");
+            Assert.AreEqual(keyValues["Key2"], "Value2");
+        }
+
+        [Test]
+        public void ToKeyValues_WhenTwoKeyValuePairsExistsWithTrailingPairDelimitor_TwoKeyValueEntriesAreCreated()
+        {
+            var stringValue = "Key1=Value1;Key2=Value2;";
+
+            var keyValues = stringValue.ToKeyValues(';', '=');
+
+            Assert.AreEqual(2, keyValues.Count);
+            Assert.AreEqual(keyValues["Key1"], "Value1");
+            Assert.AreEqual(keyValues["Key2"], "Value2");
+        }
+    }
+}
