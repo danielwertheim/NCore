caaa0b3569731eb9e5034b82c0a9044de3cfd2e5
Daniel Wertheim <daniel@wertheim.se>
2011-08-19 16:25:09 +0200

    Updated visibility of last methods.

--- Solution/Source/NCore/Collections/DictionaryExtensions.cs
+++ Solution/Source/NCore/Collections/DictionaryExtensions.cs
@@ -4,14 +4,14 @@ namespace NCore.Collections
 {
     public static class DictionaryExtensions
     {
-        internal static int GetInt(this IDictionary<string, string> kvs, string key, int defaultValue = 0)
+        public static int GetInt(this IDictionary<string, string> kvs, string key, int defaultValue = 0)
         {
             var v = GetString(kvs, key);
 
             return v != null ? int.Parse(v) : defaultValue;
         }
 
-        internal static string GetString(this IDictionary<string, string> kvs, string key, string defaultValue = null)
+        public static string GetString(this IDictionary<string, string> kvs, string key, string defaultValue = null)
         {
             return kvs.ContainsKey(key) ? kvs[key] : defaultValue;
         }
