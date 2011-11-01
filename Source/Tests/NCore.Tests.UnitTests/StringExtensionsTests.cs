924cbdc332371c78643eb10eadeb4ea05ea0763f
Daniel Wertheim <daniel@wertheim.se>
2011-11-01 20:52:39 +0100

    String extensions for PrependWith and AppendWith.

--- Source/Tests/NCore.Tests.UnitTests/StringExtensionsTests.cs
+++ Source/Tests/NCore.Tests.UnitTests/StringExtensionsTests.cs
@@ -28,5 +28,41 @@ namespace NCore.Tests.UnitTests
             Assert.AreEqual(keyValues["Key1"], "Value1");
             Assert.AreEqual(keyValues["Key2"], "Value2");
         }
+
+        [Test]
+        public void AppendWith_WhenStringHasValue_TextIsPrepended()
+        {
+            Assert.AreEqual("You did append me.", "You did".AppendWith(" append me."));
+        }
+
+        [Test]
+        public void AppendWith_WhenStringIsEmpty_TextIsNotPrepended()
+        {
+            Assert.AreEqual(string.Empty, string.Empty.AppendWith("This should not be prepended"));
+        }
+
+        [Test]
+        public void AppendWith_WhenStringIsNull_TextIsNotPrepended()
+        {
+            Assert.AreEqual(null, (null as string).AppendWith("This should not be prepended"));
+        }
+
+        [Test]
+        public void PrependWith_WhenStringHasValue_TextIsPrepended()
+        {
+            Assert.AreEqual("You did Prepend me.", "Prepend me.".PrependWith("You did "));
+        }
+
+        [Test]
+        public void PrependWith_WhenStringIsEmpty_TextIsNotPrepended()
+        {
+            Assert.AreEqual(string.Empty, string.Empty.PrependWith("This should not be prepended"));
+        }
+
+        [Test]
+        public void PrependWith_WhenStringIsNull_TextIsNotPrepended()
+        {
+            Assert.AreEqual(null, (null as string).PrependWith("This should not be prepended"));
+        }
     }
 }
