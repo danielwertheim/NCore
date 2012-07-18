8de2f45fa1c11d47d139717212faffea30a10a8a
Daniel Wertheim <daniel@wertheim.se>
2012-07-18 11:55:37 +0200

    Added Disposer.TryDispose.

---
+++ Source/Tests/NCore.Tests.UnitTests/DisposerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+
+namespace NCore.Tests.UnitTests
+{
+    [TestFixture]
+    public class DisposerTests : UnitTestBase
+    {
+        [Test]
+        public void TryDispose_WhenNullReferenceIsPassed_ReturnsNull()
+        {
+            IDisposable disposable = null;
+
+            var ex = Disposer.TryDispose(disposable);
+
+            Assert.IsNull(ex);
+        }
+
+        [Test]
+        public void TryDispose_WhenNoExceptionIsRaisedInDispose_ReturnsNull()
+        {
+            var disposableMock = new Mock<IDisposable>();
+
+            var ex = Disposer.TryDispose(disposableMock.Object);
+
+            Assert.IsNull(ex);
+            disposableMock.Verify(m => m.Dispose(), Times.Once());
+        }
+
+        [Test]
+        public void TryDispose_WhenExceptionRaisedInDispose_ReturnsException()
+        {
+            var exceptionToThrow = new Exception("Foo");
+            var disposableFake = new Mock<IDisposable>();
+            disposableFake.Setup(f => f.Dispose()).Throws(exceptionToThrow);
+
+            var ex = Disposer.TryDispose(disposableFake.Object);
+
+            Assert.IsNotNull(ex);
+            Assert.AreSame(exceptionToThrow, ex);
+        }
+    }
+}
