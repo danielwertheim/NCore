5a2859f85752f9c0d91c3d6ff40ecf91ad3189b6
Daniel Wertheim <daniel@wertheim.se>
2012-11-20 14:17:58 +0100

    Try

---
+++ Source/Projects/NCore/Try.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NCore
+{
+    public static class Try
+    {
+        public static AggregateException This(params Action[] actions)
+        {
+            var exceptions = new List<Exception>();
+
+            foreach (var action in actions)
+            {
+                try
+                {
+                    action();
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+            }
+
+            return exceptions.Any()
+                ? new AggregateException(exceptions)
+                : null;
+        }
+    }
+}
