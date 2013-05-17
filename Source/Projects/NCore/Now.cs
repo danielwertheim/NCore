e4ae25ca377dc9a9dc9706e004bee8fee0c2997a
Daniel Wertheim <daniel@wertheim.se>
2013-05-17 13:51:11 +0200

    SysDateTime is now Now.

---
+++ Source/Projects/NCore/Now.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NCore
+{
+    public static class Now
+    {
+        public static Func<DateTime> ValueFn;
+        public static Func<DateTime> UtcFn;
+ 
+        public static DateTime Value
+        {
+            get { return ValueFn.Invoke(); }
+        }
+
+        public static DateTime ValueUtc
+        {
+            get { return UtcFn.Invoke(); }
+        }
+
+        static Now()
+        {
+            Reset();
+        }
+
+        public static void Reset()
+        {
+            ValueFn = () => DateTime.Now;
+            UtcFn = () => DateTime.UtcNow;
+        }
+    }
+}
